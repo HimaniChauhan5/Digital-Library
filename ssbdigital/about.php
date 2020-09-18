408a24954d15362e19a2a5e4fcf674eb49242753
HimaniChauhan5 <himanichauhan883@gmail.com>
2020-09-18 23:33:31 +0530

    Complete first line of dialogue

---
+++ ssbdigital/about.php
@@ -0,0 +1,28 @@
+<?php 
+	$titlepage = "About Us";
+	include "./template/header.php";
+?>
+
+<div class="container">
+	<div class="panel panel-success">
+		<div class="panel-body">
+				<div class="page-header">
+		 		 	<h1>About Us</h1>
+				</div>
+				<h3>
+				Welcome
+				</h3>
+				<br>
+				<p>
+				   A digital library, digital repository, or digital collection, is an online database of digital objects that can include text, still images, audio, video, digital documents, or other digital media formats. Objects can consist of digitized content like print or photographs, as well as originally produced digital content like word processor files or social media posts. In addition to storing content, digital libraries provide means for organizing, searching, and retrieving the content contained in the collection.Digital libraries can vary immensely in size and scope, and can be maintained by individuals or organizations.The digital content may be stored locally, or accessed remotely via computer networks. These information retrieval systems are able to exchange information with each other through interoperability and sustainability.
+
+
+				</p>
+
+			<br><br><br>
+		</div>	
+	</div>
+	<br><br><br>
+</div>
+
+<?php include "./template/footer.php"; ?>
